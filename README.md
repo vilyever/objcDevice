78b14a3925c47e4ca2188c6be37c1b692a5a590f
vilyever <250450035@163.com>
2016-08-05 15:53:29 +0800

    first release

---
+++ README.md
@@ -0,0 +1,17 @@
+# objcDevice
+objcDevice
+
+## Import
+### [CocoaPods](http://cocoapods.org)
+use_frameworks!
+</br>
+pod 'objcDevice'
+
+### [Carthage](https://github.com/Carthage/Carthage)
+github "vilyever/objcDevice" "master"
+
+## Usage
+
+## License
+
+[MIT license](LICENSE)
--- objcDevice/objcDevice.h
+++ objcDevice/objcDevice.h
@@ -16,4 +16,17 @@ FOUNDATION_EXPORT const unsigned char objcDeviceVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcDevice/PublicHeader.h>
 
+#if !VDDeviceVersionCheck
+#define VDDeviceVersionCheck(version) \
+([[[UIDevice currentDevice] systemVersion] floatValue] >= version ? YES : NO)
+#endif
 
+#if !VDDeviceiOS8Check
+#define VDDeviceiOS8Check \
+([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0 ? YES : NO)
+#endif
+
+#if !VDDevicePadCheck
+#define VDDevicePadCheck \
+(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#endif
