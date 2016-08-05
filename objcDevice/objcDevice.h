f08ab1d13c2ebc60be8fced49df05e21624db5c8
vilyever <250450035@163.com>
2016-08-05 15:50:05 +0800

    Initial Commit

---
+++ objcDevice/objcDevice.h
@@ -0,0 +1,19 @@
+//
+//  objcDevice.h
+//  objcDevice
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcDevice.
+FOUNDATION_EXPORT double objcDeviceVersionNumber;
+
+//! Project version string for objcDevice.
+FOUNDATION_EXPORT const unsigned char objcDeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcDevice/PublicHeader.h>
+
+
